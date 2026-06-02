5d8f149e43e961d7bf03b9e62502270a4c9430a9
hadevelopux <harryalejandro@gmail.com>
2026-06-02 17:56:11 -0400

    Fix version docker compose

--- setup/setup_redlocal_64.sh
+++ setup/setup_redlocal_64.sh
@@ -127,7 +127,7 @@ echo -e "${GREEN}${SETUP_REDLOCAL_ROOT_OK}${NC}"
 # 2. ACTUALIZACIÓN, DEPENDENCIAS Y AUTO-PARCHEO (UNATTENDED-UPGRADES)
 echo -e "${GREEN}${SETUP_REDLOCAL_DEPS}${NC}"
 apt-get update -y
-apt-get install -y docker.io docker-compose-v2 ufw bridge-utils curl git unattended-upgrades apt-config-auto-update
+apt-get install -y docker.io docker-compose ufw bridge-utils curl git unattended-upgrades apt-config-auto-update
 
 echo -e "${GREEN}[+] Configurando actualizaciones de seguridad automáticas...${NC}"
 cat <<EOF > /etc/apt/apt.conf.d/20auto-upgrades
--- setup/setup_sensor_32.sh
+++ setup/setup_sensor_32.sh
@@ -92,7 +92,7 @@ echo -e "${GREEN}${SETUP_SENSOR_ROOT_OK}${NC}"
 # 2. ACTUALIZACIÓN E INSTALACIÓN DE DEPENDENCIAS Y AUTO-PARCHEO
 echo -e "${GREEN}${SETUP_SENSOR_DEPS}${NC}"
 apt-get update -y
-apt-get install -y ufw docker.io docker-compose-v2 git curl rfkill tcpdump tshark unattended-upgrades apt-config-auto-update
+apt-get install -y ufw docker.io docker-compose git curl rfkill tcpdump tshark unattended-upgrades apt-config-auto-update
 
 echo -e "${GREEN}[+] Configurando actualizaciones de seguridad automáticas...${NC}"
 cat <<EOF > /etc/apt/apt.conf.d/20auto-upgrades
